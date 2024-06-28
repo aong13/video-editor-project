3be9b9e1c79ad6cda95ad211767fe794d6ad7095
aong13 <oaoa13@pukyong.ac.kr>
2024-06-29 01:04:54 +0900

    feat: 홈화면 라우팅

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,27 +9,7 @@ const Home = () => {
   };
   return (
     <Container>
-      <Row className="justify-content-center mt-5">
-        <Col xs={12} md={6} className="text-center">
-          <h1>BootStrap, Antd 실습</h1>
-          <div className="d-flex justify-content-around mt-4">
-            <Button
-              variant="primary"
-              onClick={() => onNavigate("/cms")}
-              style={{ width: "150px" }}
-            >
-              CMS
-            </Button>
-            <Button
-              variant="secondary"
-              onClick={() => onNavigate("/video")}
-              style={{ width: "150px" }}
-            >
-              Video
-            </Button>
-          </div>
-        </Col>
-      </Row>
+      <div>홈화면</div>
     </Container>
   );
 };
