fcbc2654c99ff0065545b0f4db3d5d4fdcbc39be
aong13 <oaoa13@pukyong.ac.kr>
2024-06-29 01:04:54 +0900

    feat: 홈화면 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PrivateRoute from "./route/PrivateRoute";
 import VideoEditor from "./pages/VideoEditor/VideoEditor";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/*" element={<PrivateRoute />}>
+          <Route path="" element={<Home />} />
           <Route path="edit-video" element={<VideoEditor />} />
         </Route>
       </Routes>
