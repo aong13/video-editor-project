71214c4c587201c939ad29d91aec45c174df8950
aong13 <oaoa13@pukyong.ac.kr>
2024-07-03 07:13:29 +0900

    fix: vercel 배포를 위해 header 추가

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,21 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  headers: [
+    {
+      source: "/(.*)",
+      headers: [
+        {
+          key: "cross-origin-opener-policy",
+          value: "same-origin",
+        },
+        {
+          key: "cross-origin-embedder-policy",
+          value: "require-corp",
+        },
+      ],
+    },
+  ],
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
