c093a058c79040efd46a12799d5c23555679a3d6
aong13 <oaoa13@pukyong.ac.kr>
2024-07-03 06:45:22 +0900

    route: 기본 경로 설정 접속 시 바로 edit-video로 이동

--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -1,8 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Header from "../components/Layout/Header";
 import Footer from "../components/Layout/Footer";
+import { useEffect, useState } from "react";
 
 const PrivateRoute = () => {
+  const { pathname } = useLocation();
+  const [, main, sub] = pathname.split("/");
+  const selected = sub ? [sub, main] : [main];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    !selected[0] && navigate("/edit-video");
+  }, [navigate, selected]);
+
   return (
     <div>
       <Header />
