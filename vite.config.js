adebd0b2475eab045048527393c96c79fd2c7d21
aong13 <oaoa13@pukyong.ac.kr>
2024-07-03 07:18:47 +0900

    fix: vercel.json 추가 vercel배포 시 오류 해결을 위함

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  headers: [
-    {
-      source: "/(.*)",
-      headers: [
-        {
-          key: "cross-origin-opener-policy",
-          value: "same-origin",
-        },
-        {
-          key: "cross-origin-embedder-policy",
-          value: "require-corp",
-        },
-      ],
-    },
-  ],
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
